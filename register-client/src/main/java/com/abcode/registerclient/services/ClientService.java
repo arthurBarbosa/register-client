198ed6a0ff779fa1a5f1c4c93512dd5a77508c16
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-15 10:57:42 -0400

    GET client by id

--- register-client/src/main/java/com/abcode/registerclient/services/ClientService.java
+++ register-client/src/main/java/com/abcode/registerclient/services/ClientService.java
@@ -2,6 +2,7 @@ package com.abcode.registerclient.services;
 
 import com.abcode.registerclient.dto.ClientDTO;
 import com.abcode.registerclient.repositories.ClientRepository;
+import com.abcode.registerclient.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,7 +17,13 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
-        var listPageable =  repository.findAll(pageRequest);
+        var listPageable = repository.findAll(pageRequest);
         return listPageable.map(ClientDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        var entityClient = repository.findById(id);
+        return new ClientDTO(entityClient.orElseThrow(() -> new ResourceNotFoundException("Entity not found")));
     }
 }
---
+++ register-client/src/main/java/com/abcode/registerclient/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.abcode.registerclient.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
