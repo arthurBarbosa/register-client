22763686498458017dd885146529b5ee49aca391
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-15 09:37:41 -0400

    Seeding data

---
+++ register-client/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('João Rosa Junior', '15862341021', 4555.0, TIMESTAMP WITH TIME ZONE '1992-01-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('Estevam Rodrigues', '58237991010', 4555.0, TIMESTAMP WITH TIME ZONE '1992-01-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('Celio Matos', '56054240099', 4555.0, TIMESTAMP WITH TIME ZONE '1992-01-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('André Cavalcante Machado', '82052502009', 4555.0, TIMESTAMP WITH TIME ZONE '1992-01-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('Renato Bartholo', '93000703047', 4555.0, TIMESTAMP WITH TIME ZONE '1985-01-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('Caio Arthur', '51670887049', 4555.0, TIMESTAMP WITH TIME ZONE '1992-01-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('Cleber Nascimento', '52761103017', 4555.0, TIMESTAMP WITH TIME ZONE '1993-02-1T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('William Gomes', '56332665017', 4555.0, TIMESTAMP WITH TIME ZONE '1988-01-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('João Menna', '21440790051', 4555.0, TIMESTAMP WITH TIME ZONE '1991-03-14T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children ) VALUES ('Giovanni Maciel', '06855062075', 4555.0, TIMESTAMP WITH TIME ZONE '1994-02-20T10:00:00Z', 1);
