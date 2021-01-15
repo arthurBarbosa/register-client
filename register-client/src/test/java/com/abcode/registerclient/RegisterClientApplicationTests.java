4332d9d462b5624463c8242e3e27d6b0d8277178
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-15 08:24:25 -0400

    Project created

---
+++ register-client/src/test/java/com/abcode/registerclient/RegisterClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcode.registerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
