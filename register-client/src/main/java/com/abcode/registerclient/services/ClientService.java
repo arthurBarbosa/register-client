025f5e2272bfcc1995f2605ab7d758e237a08fa0
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-15 12:59:18 -0400

    DELETE Client

--- register-client/src/main/java/com/abcode/registerclient/services/ClientService.java
+++ register-client/src/main/java/com/abcode/registerclient/services/ClientService.java
@@ -5,6 +5,7 @@ import com.abcode.registerclient.entities.Client;
 import com.abcode.registerclient.repositories.ClientRepository;
 import com.abcode.registerclient.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -45,10 +46,20 @@ public class ClientService {
             copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new ClientDTO(entity);
-        }
-        catch (EntityNotFoundException e) {
+        } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
         }
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+
     }
 
     private void copyDtoToEntity(ClientDTO dto, Client entity) {
